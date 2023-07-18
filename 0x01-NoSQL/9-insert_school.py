e534af27d1e742732a4f03d6cdf1e5ffbf3258f3
KayongoYongo <sknsammie10@gmail.com>
2023-07-18 16:56:11 +0000

    insert school

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""The script inserts a document into a collection"""
+
+
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Returns the document id"""
+    # Insert the document into the collection
+    result = mongo_collection.insert_one(kwargs)
+    
+    # Return the new document's _id
+    return result.inserted_id
