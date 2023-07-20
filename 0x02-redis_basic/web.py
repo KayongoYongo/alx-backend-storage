c622eecb91fda98eccc7768d4cd725666c8726db
KayongoYongo <sknsammie10@gmail.com>
2023-07-20 23:56:29 +0000

    update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,40 +1,36 @@
 #!/usr/bin/env python3
-"""Implementing an expiring web cache and tracker"""
-
-
+"""
+web cache and tracker
+"""
 import requests
 import redis
-import time
+from functools import wraps
+
+store = redis.Redis()
 
 
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
+    @wraps(method)
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
+    return wrapper
+
+
+@count_url_access
 def get_page(url: str) -> str:
-    """Implient expiring web cache and tracker"""
-    r = redis.Redis()
-
-    # Create a key to track the access count for the URL
-    count_key = f"count:{url}"
-
-    # Check if the URL content is already cached
-    cached_content = r.get(url)
-    if cached_content:
-        # URL content is cached, return it
-        r.incr(count_key)  # Increment the access count
-        return cached_content.decode("utf-8")
-
-    # URL content is not cached, fetch it from the web
-    response = requests.get(url)
-    if response.status_code == 200:
-        # Cache the content with a 10-second expiration time
-        r.setex(url, 10, response.content)
-
-        # Increment the access count
-        r.incr(count_key)
-
-        return response.text
-
-    return ""  # Return an empty string if the request was unsuccessful
-
-
-# Test the get_page function
-url = "http://slowwly.robertomurray.co.uk"
-print(get_page(url))
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
